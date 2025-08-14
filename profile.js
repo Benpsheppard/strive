a81f5258a2afa3b439f35bdd6b0cfdcbb00978a5
Benpsheppard <ben@bensheppard.co.uk>
2025-08-14 22:31:41 +0100

    General updates to HTML and JS

--- profile.js
+++ profile.js
@@ -1,4 +1,7 @@
+document.getElementById('currentUser').textContent = localStorage.getItem('currentUser') || 'Guest'; // Display current user
+
 document.getElementById("logoutBtn").addEventListener("click", function() {
     localStorage.removeItem("currentUser"); // Clear the current user from localStorage
     window.location.href = "login.html"; // Redirect to the login page
 });
+
