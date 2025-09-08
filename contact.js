725cc65d5d601dcf5533e3195faa03d6a2c9c0fe
Benpsheppard <ben@bensheppard.co.uk>
2025-09-08 13:42:54 +0100

    File organisation updated

--- contact.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const currentUser = localStorage.getItem('currentUser');    // Get the current user from localStorage
-
-    if (!currentUser) {
-        window.location.href = "login.html"; // Redirect to login if no user is logged in
-        return;
-    }
-    document.getElementById('currentUser').textContent = currentUser; // Display current user
-});
