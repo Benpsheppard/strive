0c76ea4fca61d88e885de9cd2c268bd523e2137a
Benpsheppard <ben@bensheppard.co.uk>
2025-10-16 19:59:08 +0100

    Updated to be mobile friendly

--- strive-frontend/src/components/GuestHeader.jsx
+++ strive-frontend/src/components/GuestHeader.jsx
@@ -2,21 +2,20 @@
 const GuestHeader = ({ currentWorkouts }) => {
     const maxWorkouts = 5;
     const workoutsLeft = maxWorkouts - currentWorkouts;
-    const progressPercentage = (currentWorkouts / maxWorkouts) * 100;
 
     return (
-        <div className="w-full bg-[#EF233C] text-[#EDF2F4] font-semibold px-4 py-1 flex flex-col items-center text-sm font-medium shadow-md">
-            <div className="flex justify-center items-center">
-                <span className="mr-2">
-                    You are using a Guest account: {currentWorkouts}/{maxWorkouts} workouts completed.
+        <div className="w-full bg-[#EF233C] text-[#EDF2F4] font-semibold px-4 py-3 flex flex-col items-center text-sm font-medium shadow-md">
+            <div className="flex flex-col sm:flex-row justify-center items-center text-center gap-1 sm:gap-0">
+                <span className="sm:mr-2">
+                    Guest account: {currentWorkouts}/{maxWorkouts} workouts
                 </span>
                 {currentWorkouts >= maxWorkouts ? (
-                    <span className="py-2 px-2">
+                    <span className="sm:py-2 sm:px-2">
                         Upgrade to a free Strive account for unlimited workouts!
                     </span>
                 ) : (
-                    <span className="py-2 px-2">
-                        You can do {workoutsLeft} more workout{workoutsLeft > 1 ? 's' : ''} before upgrading for free.
+                    <span className="sm:py-2 sm:px-2">
+                        You can do {workoutsLeft} more workout{workoutsLeft > 1 ? 's' : ''} before upgrading.
                     </span>
                 )}
             </div>
