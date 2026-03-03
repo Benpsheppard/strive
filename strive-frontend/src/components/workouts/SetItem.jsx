f8757718289452acef510600e3722173876a2a2d
Benpsheppard <ben@bensheppard.co.uk>
2026-03-03 12:14:47 +0000

    Separated set item into it's own component

---
+++ strive-frontend/src/components/workouts/SetItem.jsx
@@ -0,0 +1,25 @@
+// SetItem.jsx
+
+// Imports
+import { motion } from 'framer-motion'
+import { formatWeight } from '../../utils/weightUnits.js'
+
+const SetItem = ({ set, local, useImperial = false }) => {
+
+    const onDelete = () => {
+        
+    }
+
+    return (
+        <>
+            <motion.li initial={{ opacity: 0, y: -5 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, scale: 0.9 }} className="bg-[#2B2D42] text-[#EDF2F4] border border-[#EDF2F4]/30 rounded-full px-3 py-1 text-sm shadow-sm">
+                <span className="text-[#EF233C] font-semibold">
+                    {formatWeight(set.weight, useImperial)}
+                </span>{' '}
+                × {set.reps} reps
+            </motion.li>
+        </>
+    )
+}
+
+export default SetItem
--- strive-frontend/src/components/workouts/SetList.jsx
+++ strive-frontend/src/components/workouts/SetList.jsx
@@ -1,9 +1,11 @@
 // SetList.jsx
 
 // Imports
-import { motion, AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 
-// Function Imports
+// Function / Component Imports
+import SetItem from './SetItem.jsx'
+
 import { formatWeight } from '../../utils/weightUnits.js'
 
 const SetList = ({ sets, useImperial = false }) => {
@@ -15,9 +17,7 @@ const SetList = ({ sets, useImperial = false }) => {
         <AnimatePresence>
             <ul className="flex flex-wrap justify-center gap-2 mb-2">
                 {sets.map((s, i) => (
-                    <motion.li key={i} initial={{ opacity: 0, y: -5 }} animate={{ opacity: 1, y: 0 }} exit={{ opacity: 0, scale: 0.9 }} className="bg-[#2B2D42] text-[#EDF2F4] border border-[#EDF2F4]/30 rounded-full px-3 py-1 text-sm shadow-sm">
-                        <span className="text-[#EF233C] font-semibold">{formatWeight(s.weight, useImperial)}</span> × {s.reps} reps
-                    </motion.li>
+                    <SetItem key={i} set={s} useImperial={useImperial} />
                 ))}
             </ul>
         </AnimatePresence>
