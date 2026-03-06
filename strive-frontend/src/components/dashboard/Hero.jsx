1e722d9ed54981788b2e1144b53777c1a872c58c
Benpsheppard <ben@bensheppard.co.uk>
2026-03-06 18:22:48 +0000

    Removed tagline

--- strive-frontend/src/components/dashboard/Hero.jsx
+++ strive-frontend/src/components/dashboard/Hero.jsx
@@ -13,9 +13,6 @@ const Hero = () => {
                 <h1 className="title text-8xl md:text-6xl font-bold text-[#EDF2F4] mb-4 drop-shadow-lg">
                     STRIVE
                 </h1>
-                <p className="tag-line text-xl md:text-2xl text-[#EDF2F4] mb-6 drop-shadow-md">
-                    Strive for better
-                </p>
                 <button className="bg-[#EF233C] hover:bg-red-700 text-[#EDF2F4] font-semibold py-3 px-6 rounded-lg shadow-lg transition duration-300">
                     <Link to={user ? ('/new-workout') : ('/login')}>Let's Get Started</Link>
                 </button>
