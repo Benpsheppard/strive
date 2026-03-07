755c59381c2ea613f86fd79a7aa6b5ec29898c67
Benpsheppard <ben@bensheppard.co.uk>
2026-03-07 15:55:16 +0000

    Updated layout for mobile

--- strive-frontend/src/components/dashboard/Hero.jsx
+++ strive-frontend/src/components/dashboard/Hero.jsx
@@ -8,9 +8,9 @@ const Hero = () => {
     const { user } = useSelector((state) => state.auth)
 
     return (
-        <section className="hero bg-[#8D99AE] h-screen flex items-center justify-center">
-            <div className="hero-container text-center px-6">
-                <h1 className="title text-8xl md:text-6xl font-bold text-[#EDF2F4] mb-4 drop-shadow-lg">
+        <section className="bg-[#8D99AE] h-screen flex items-center justify-center -mt-20 md:mt-0">
+            <div className="text-center px-6">
+                <h1 className="text-8xl md:text-9xl font-bold text-[#EDF2F4] mb-4 drop-shadow-lg">
                     STRIVE
                 </h1>
                 <button className="bg-[#EF233C] hover:bg-red-700 text-[#EDF2F4] font-semibold py-3 px-6 rounded-lg shadow-lg transition duration-300">
