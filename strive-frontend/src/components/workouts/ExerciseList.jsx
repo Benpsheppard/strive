14373fb4fc7b613c0f9a1e1e51d4c708d087fd19
Benpsheppard <ben@bensheppard.co.uk>
2026-03-17 10:57:14 +0000

    Updated layout

--- strive-frontend/src/components/workouts/ExerciseList.jsx
+++ strive-frontend/src/components/workouts/ExerciseList.jsx
@@ -4,27 +4,33 @@
 import { formatWeight } from '../../utils/formatValues.js'
 
 const ExerciseList = ({ exercises, useImperial }) => {
-    if (!exercises || exercises.length === 0) return null
+    if (!exercises || exercises.length === 0) {
+        return null
+    }
 
     return (
         <div className="max-h-64 overflow-y-auto mb-4">
-        {exercises.map((ex, i) => (
-            <div key={i} className="bg-[#8D99AE] p-2 rounded-lg mb-2 text-center shadow-lg">
-            <h4 className="font-bold text-[#EF233C]">{ex.name}</h4>
-            <p className="text-[#EDF2F4]">
-                {ex.musclegroup} — {ex.description}
-            </p>
-            {ex.sets && ex.sets.length > 0 && (
-                <ul>
-                {ex.sets.map((s, idx) => (
-                    <li className="text-[#2B2D42]" key={idx}>
-                    - {formatWeight(s.weight, useImperial)} × {s.reps} reps
-                    </li>
-                ))}
-                </ul>
-            )}
-            </div>
-        ))}
+            {exercises.map((ex, i) => (
+                <div key={i} className="bg-[#8D99AE] p-2 rounded-lg mb-2 text-center shadow-lg">
+                <h4 className="font-bold text-[#EF233C]">
+                    {ex.name}
+                </h4>
+
+                <p className="text-[#EDF2F4]">
+                    {ex.musclegroup} {ex.description && `— ${ex.description}`} 
+                </p>
+
+                {ex.sets && ex.sets.length > 0 && (
+                    <ul>
+                        {ex.sets.map((s, idx) => (
+                            <li className="text-[#2B2D42]" key={idx}>
+                                {formatWeight(s.weight, useImperial)} × {s.reps} reps
+                            </li>
+                        ))}
+                    </ul>
+                )}
+                </div>
+            ))}
         </div>
     )
 }
