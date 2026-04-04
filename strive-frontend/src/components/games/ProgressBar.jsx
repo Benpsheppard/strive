6a0fd633bcf7e1c8fd8ae36a66bd63424dc8c684
Benpsheppard <ben@bensheppard.co.uk>
2026-04-04 16:39:57 +0100

    Added border

--- strive-frontend/src/components/games/ProgressBar.jsx
+++ strive-frontend/src/components/games/ProgressBar.jsx
@@ -20,7 +20,7 @@ const ProgressBar = () => {
 
     return (
         <div className='mt-4'>
-            <div className='relative w-full bg-[#2B2D42] rounded-full h-6 overflow-hidden'>
+            <div className='relative w-full bg-[#2B2D42] rounded-full h-6 overflow-hidden border-2 border-[#EDF2F4]'>
 
                 {/* Fill */}
                 <div className='bg-[#EF233C] h-6 transition-all duration-500 ease-out' style={{ width: `${progressPercentage}%` }} />
