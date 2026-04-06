edc5b2b4bcb5a37738787966895a5d625a6f2487
Benpsheppard <ben@bensheppard.co.uk>
2026-04-06 19:04:16 +0100

    Updated

--- strive-backend/server.js
+++ strive-backend/server.js
@@ -1,9 +1,8 @@
 // server.js
-// File to run server for Strive
 
 // Imports
-const express = require('express')                                     
-const dotenv = require('dotenv').config()                             
+require('dotenv').config()
+const express = require('express')                                                       
 const colors = require('colors')                                   
 const path = require('path')                                          
 const helmet = require('helmet')  
