2ccf49c15fa6ffe45719d6f03f9e3f82bce337d2
Benpsheppard <ben@bensheppard.co.uk>
2026-04-06 22:02:22 +0100

    Added date formatting for new updateStreak function

--- strive-backend/utils/dateFormat.js
+++ strive-backend/utils/dateFormat.js
@@ -25,16 +25,33 @@ export function getStartOfWeek(date) {
 }
 
 export function getEndOfWeek(date) {
-  const start = getStartOfWeek(date)
+	const start = getStartOfWeek(date)
 
-  const end = new Date(start)
-  end.setDate(end.getDate() + 6)
-  end.setHours(23, 59, 59, 999)
+	const end = new Date(start)
+	end.setDate(end.getDate() + 6)
+	end.setHours(23, 59, 59, 999)
 
-  return end
+	return end
 }
 
-const now = new Date()
-console.log(`Current week number for today: ${getWeekNumber(now)}`)
-console.log(`Start of the week: ${getStartOfWeek(now)}`)
-console.log(`End of the week: ${getEndOfWeek(now)}`)
+export function getISOWeekString(date = new Date()) {
+    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    const week = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    return `${d.getUTCFullYear()}-W${String(week).padStart(2, '0')}`;
+}
+
+export function getWeeksBetween(fromWeek, toWeek) {
+	const toDate = isoWeekToDate(toWeek);
+	const fromDate = isoWeekToDate(fromWeek);
+	return Math.floor((toDate - fromDate) / (7 * 24 * 60 * 60 * 1000));
+}
+
+export function isoWeekToDate(weekStr) {
+	const [year, week] = weekStr.split('-W').map(Number);
+	const jan4 = new Date(Date.UTC(year, 0, 4));
+	const startOfWeek1 = new Date(jan4);
+	startOfWeek1.setUTCDate(jan4.getUTCDate() - (jan4.getUTCDay() || 7) + 1);
+	return new Date(startOfWeek1.getTime() + (week - 1) * 7 * 24 * 60 * 60 * 1000);
+}
