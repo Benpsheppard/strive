1a008f2020d27423b09c0012872162162eb21a8f
Benpsheppard <ben@bensheppard.co.uk>
2026-04-13 20:18:13 +0100

    Separated muscle group info into file

---
+++ strive-frontend/src/utils/muscleGroupUtils.js
@@ -0,0 +1,24 @@
+// muscleGroupUtils.js
+
+export const MUSCLE_GROUPS = [
+    'All',
+    'Chest',
+    'Back',
+    'Shoulders',
+    'Arms',
+    'Legs',
+    'Core',
+    'Full body',
+    'Other'
+]
+
+export const MUSCLE_GROUP_COLOURS = {
+    'Chest': '#EF233C',
+    'Back': '#D90429',
+    'Shoulders': '#8D99AE',
+    'Arms': '#2B2D42',
+    'Legs': '#4ECDC4',
+    'Core': '#F4A261',
+    'Full body': '#6A4C93',
+    'Other': '#EDF2F4',
+}
