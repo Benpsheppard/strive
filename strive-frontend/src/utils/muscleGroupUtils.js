2b726b626d68287d7b454b93018e46c1a75cc409
Benpsheppard <ben@bensheppard.co.uk>
2026-04-14 22:55:12 +0100

    Updated colours

--- strive-frontend/src/utils/muscleGroupUtils.js
+++ strive-frontend/src/utils/muscleGroupUtils.js
@@ -15,10 +15,10 @@ export const MUSCLE_GROUPS = [
 export const MUSCLE_GROUP_COLOURS = {
     'Chest': '#EF233C',
     'Back': '#D90429',
-    'Shoulders': '#8D99AE',
+    'Shoulders': '#EDF2F4',
     'Arms': '#2B2D42',
     'Legs': '#4ECDC4',
     'Core': '#F4A261',
     'Full body': '#6A4C93',
-    'Other': '#EDF2F4',
+    'Other': '#8D99AE',
 }
