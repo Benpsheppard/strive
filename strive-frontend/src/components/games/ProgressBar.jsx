92e188d1e02507949e798c151bf8d86454dc5808
Benpsheppard <ben@bensheppard.co.uk>
2026-04-14 16:50:32 +0100

    Increased progress bar size

--- strive-frontend/src/components/games/ProgressBar.jsx
+++ strive-frontend/src/components/games/ProgressBar.jsx
@@ -2,10 +2,10 @@
 
 const ProgressBar = ({ progressPercentage, numerator, denominator }) => {
     return (
-        <div className='relative w-full bg-[#2B2D42] rounded-full h-6 overflow-hidden border-2 border-[#EDF2F4]'>
+        <div className='relative w-full bg-[#2B2D42] rounded-xl h-8 overflow-hidden border-2 border-[#EDF2F4]'>
 
             {/* Fill */}
-            <div className='bg-[#EF233C] h-6 transition-all duration-500 ease-out' style={{ width: `${progressPercentage}%` }} />
+            <div className='bg-[#EF233C] h-8 transition-all duration-500 ease-out' style={{ width: `${progressPercentage}%` }} />
 
             {/* Text Overlay */}
             <div className='absolute inset-0 flex items-center justify-center'>
