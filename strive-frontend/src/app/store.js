ac576f9650d638c2a94777e2908cb48881b20fac
Benpsheppard <ben@bensheppard.co.uk>
2026-04-27 16:42:13 +0100

    Added leaderboard reducer

--- strive-frontend/src/app/store.js
+++ strive-frontend/src/app/store.js
@@ -3,12 +3,14 @@ import authReducer from '../features/auth/authSlice.js'
 import workoutReducer from '../features/workouts/workoutsSlice.js'
 import questReducer from '../features/quests/questSlice.js'
 import exerciseReducer from '../features/exercises/exerciseSlice.js'
+import leaderboardReducer from '../features/leaderboard/leaderboardSlice.js'
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
         workout: workoutReducer,
         quest: questReducer,
-        exercise: exerciseReducer
+        exercise: exerciseReducer,
+        leaderboard: leaderboardReducer
     }
 })
