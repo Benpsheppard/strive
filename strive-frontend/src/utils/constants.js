d58e1fbeae3603cb80b136e9b4afbff677b7ce73
bensheppard <ben@bensheppard.co.uk>
2026-05-19 14:46:04 +0100

    Moved empty set and empty exercise into its own file

---
+++ strive-frontend/src/utils/constants.js
@@ -0,0 +1,20 @@
+// constants.js
+
+export const EMPTY_EXERCISE = {
+    exerciseId: null,
+    exerciseName: '',
+    muscleGroup: '',
+    trackingMode: '',
+    equipment: [],
+    selectedEquipment: '',
+    sets: []
+}
+
+export const EMPTY_SET = {
+    weight: '', 
+    reps: '', 
+    duration: '', 
+    distance: '', 
+    addedWeight: '', 
+    assistance: ''
+}
