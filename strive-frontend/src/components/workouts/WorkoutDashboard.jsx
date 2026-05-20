a994c50749d0e4dec969f9d21f4bba608fe23aa6
bensheppard <ben@bensheppard.co.uk>
2026-05-20 13:09:05 +0100

    Added 'last three workouts' and updated layout

--- strive-frontend/src/components/workouts/WorkoutDashboard.jsx
+++ strive-frontend/src/components/workouts/WorkoutDashboard.jsx
@@ -8,9 +8,10 @@ import MuscleGroupHeatmap from "../progress/MuscleGroupHeatmap"
 
 const WorkoutDashBoard = ({ user, workouts, startWorkout }) => {
     const lastWorkout = workouts.length > 0 ? workouts[workouts.length - 1] : null
+    const lastThreeWorkouts = workouts.length > 0 ? [...workouts].sort((a, b) => new Date(b.date) - new Date(a.date)).slice(0, 3) : []
 
     return (
-        <section className="space-y-3 w-full max-w-2xl">
+        <section className="space-y-5 w-full">
             <div className="text-4xl md:text-6xl font-semibold text-[#EDF2F4] text-center p-4">
                 <h1>Welcome back, <span className="text-[#EF233C]">{user.isGuest ? 'Guest' : user.username}</span></h1>
             </div>
@@ -21,7 +22,7 @@ const WorkoutDashBoard = ({ user, workouts, startWorkout }) => {
                 <GamesSummary user={user} />
             </div>
 
-            <div className="card-theme fade-in-card p-6 w-full sm:max-w-2xl mx-auto bg-[#8D99AE] shadow rounded-2xl" style={{ animationDelay: '0.4s' }}>
+            <div className="card-theme fade-in-card p-6 w-full mx-auto bg-[#8D99AE] rounded-2xl" style={{ animationDelay: '0.4s' }}>
                 <h2 className="text-[#EDF2F4] text-xl text-center mb-3">Ready to train?</h2>
                 <button onClick={startWorkout} className="w-full bg-[#EF233C] text-[#EDF2F4] py-2 px-4 rounded-xl hover:bg-[#D90429]">
                     Start Workout
@@ -31,16 +32,25 @@ const WorkoutDashBoard = ({ user, workouts, startWorkout }) => {
             <div className="fade-in-card" style={{ animationDelay: '0.6s' }}>
                 <Calendar workouts={workouts} />
             </div>
-            
 
-            {lastWorkout && (
-                <div className="fade-in-card" style={{ animationDelay: '0.8s' }}>
-                    <WorkoutItem workout={lastWorkout} />
+            <div className="grid grid-cols-1 md:grid-cols-2 w-full gap-5">
+                <div className="fade-in-card flex flex-col items-center w-full space-y-5 p-2 bg-[#8D99AE] text-[#EDF2F4] text-2xl font-bold rounded-2xl" style={{ animationDelay: '0.8s' }}>
+                    <h1>
+                        Most Recent <span className="text-[#EF233C]">Workouts</span>
+                    </h1>
+
+                    {lastThreeWorkouts && (
+                        lastThreeWorkouts.map((workout) => 
+                            <div key={workout.id} className="w-full">
+                                <WorkoutItem workout={workout} />
+                            </div>
+                        )
+                    )}
                 </div>
-            )}
-            
-            <div className="fade-in-card" style={{ animationDelay: '1.0s' }}>
-                <MuscleGroupHeatmap workouts={workouts} />
+
+                <div className="fade-in-card" style={{ animationDelay: '1.0s' }}>
+                    <MuscleGroupHeatmap workouts={workouts} />
+                </div>
             </div>
         </section>
     )
