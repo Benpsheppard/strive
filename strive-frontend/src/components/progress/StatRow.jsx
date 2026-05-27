41b2e1fa77fba5c51bd57b53a6803ebf29928968
bensheppard <ben@bensheppard.co.uk>
2026-05-27 12:41:06 +0100

    Updated layout and added percentage change

--- strive-frontend/src/components/progress/StatRow.jsx
+++ strive-frontend/src/components/progress/StatRow.jsx
@@ -4,37 +4,77 @@
 import { FaArrowUp, FaArrowDown, FaMinus } from 'react-icons/fa'
 
 const StatRow = ({ label, value, current, previous }) => {  
-    const ComparisonArrow = ({ current, previous }) => {
+
+    const Comparison = ({ current, previous }) => {
+        const change = calculatePercentageChange(current, previous)
+
         if (current > previous) {
             return (
-                <FaArrowUp className="text-green-400 text-sm ml-2" />
+                <span className="flex items-center ml-2 text-green-400 text-sm">
+                    {change}
+                    <FaArrowUp className="ml-1" />
+                </span>
             )
         }
 
         if (current < previous) {
             return (
-                <FaArrowDown className="text-red-400 text-sm ml-2" />
+                <span className="flex items-center ml-2 text-[#EF233C]/80 text-sm">
+                    {change}
+                    <FaArrowDown className="ml-1" />
+                </span>
             )
         }
 
         return (
-            <FaMinus className="text-[#EDF2F4]/50 text-sm ml-2" />
+            <span className="flex items-center ml-2 text-[#EDF2F4]/50 text-sm">
+                {change}
+                <FaMinus className="ml-1" />
+            </span>
         )
     }
 
+    const calculatePercentageChange = (current, previous) => {
+        if (previous === 0) {
+            if (current === 0) {
+                return '0%'
+            }
+
+            return 'New!'
+        }
+
+        let change = Math.round(((current - previous) / previous) * 100)
+
+        if (change > 999) {
+            change = 999
+        }
+
+        if (change > 0) {
+            return `+${change}%`
+        } else if (change < 0) {
+            return `-${change}%`
+        } else {
+            return `0%`
+        }
+    }
+
     return (
-        <p className="flex justify-between items-center border-b border-[#EDF2F4]/40">
-            <span>{label}</span>
+        <div className="flex flex-row justify-between items-center border-b border-[#EDF2F4]/40">
+            <p>
+                {label}
+            </p>
 
-            <span className="flex items-center text-[#EF233C] font-bold">
-                {value}
-
-                <ComparisonArrow
+            <div className="flex flex-row justify-between items-center text-[#EF233C] font-bold">
+                <p>
+                    {value}
+                </p>
+                
+                <Comparison
                     current={current}
                     previous={previous}
                 />
-            </span>
-        </p>
+            </div>
+        </div>
     )
 }
 
