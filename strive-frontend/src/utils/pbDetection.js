7bd6cb9846b9a0654cef627869c785e050666ef1
bensheppard <ben@bensheppard.co.uk>
2026-07-02 16:16:48 +0100

    Updated to detect PBs for each equipment

--- strive-frontend/src/utils/pbDetection.js
+++ strive-frontend/src/utils/pbDetection.js
@@ -9,21 +9,24 @@ export const calculatePersonalBests = (workouts) => {
         workout.exercises.forEach((ex) => {
             const exerciseData = ex.exercise
             if (!exerciseData || typeof exerciseData !== 'object') {
-				return
-			}
+                return
+            }
 
             const name = exerciseData.name
             const muscleGroup = exerciseData.muscleGroup || 'Other'
             const trackingMode = exerciseData.trackingMode
+            const equipment = ex.selectedEquipment || 'Unspecified'
 
             if (trackingMode !== 'weight_reps') {
-				return
-			}
+                return
+            }
+
+            const key = `${name}||${equipment}`
 
             ex.sets.forEach((set) => {
                 const weight = Number(set.weight) || 0
-                if (!exercisePBs[name] || weight > exercisePBs[name].weight) {
-                    exercisePBs[name] = { weight, date: workoutDate, muscleGroup }
+                if (!exercisePBs[key] || weight > exercisePBs[key].weight) {
+                    exercisePBs[key] = { weight, date: workoutDate, muscleGroup, equipment, name }
                 }
             })
         })
